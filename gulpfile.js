6619fddc4197e67c6a2c7a2fdabac8b33a2811d9
Sebastian <sebastian.neumair@gmail.com>
2022-08-27 20:55:08 +0200

    add terser to reduce file size

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,28 @@
 const gulp = require("gulp");
-const ts = require("gulp-typescript");
-const tsProject = ts.createProject("tsconfig.json");
+const browserify = require("browserify");
+const source = require("vinyl-source-stream");
+const terser = require("gulp-terser");
+const tsify = require("tsify");
+const sourcemaps = require("gulp-sourcemaps");
+const buffer = require("vinyl-buffer");
 
-gulp.task("default", function () {
-  return tsProject.src().pipe(tsProject()).js.pipe(gulp.dest("dist"));
-});
+gulp.task("default", bundle);
+
+function bundle() {
+  return browserify({
+    basedir: ".",
+    debug: true,
+    entries: ["src/main.ts"],
+    cache: {},
+    packageCache: {},
+  })
+    .plugin(tsify)
+    .bundle()
+    .on("error", fancy_log)
+    .pipe(source("bundle.js"))
+    .pipe(buffer())
+    .pipe(sourcemaps.init({ loadMaps: true }))
+    .pipe(terser())
+    .pipe(sourcemaps.write("./"))
+    .pipe(gulp.dest("dist"));
+}
